26c3f8c83072ac8618adf57470aedda98dd577fe
Samuel Dias Oliveira <samueldias004@gmail.com>
2024-04-30 16:38:34 -0300

    feat: uso getTasks - tela home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,34 @@
+import 'package:client/services/task_service.dart';
 import 'package:flutter/material.dart';
 
 import '../services/auth_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final AuthService authService = AuthService();
+  final TaskService taskService = TaskService();
+  List<dynamic> _tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAndSetTasks();
+  }
+
+  Future<void> fetchAndSetTasks() async {
+    try {
+      final tasks = await taskService.getTasks();
+      setState(() {
+        _tasks = tasks;
+      });
+    } catch (error) {
+      print('Erro ao buscar tarefas: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +39,25 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: _tasks.length,
         itemBuilder: (context, index) {
           return ListTile(
             leading: Icon(Icons.circle, color: Colors.red),
-            title: Text('Tarefa'),
-            subtitle: Text('Descrição'),
+            title: Text(_tasks[index]['title']),
+            subtitle: Text(_tasks[index]['description']),
           );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.list),
             label: 'Tarefas',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,22 +1,67 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthService {
+import 'auth_service.dart';
+
+class TaskService {
   final String baseUrl = 'http://10.0.2.2:8080';
+  final AuthService authService = AuthService();
 
-  Future<String?> getToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('token');
-  }
-
-  Future<void> someApiCall() async {
-    final token = await getToken();
+  Future<List<dynamic>> getTasks() async {
+    final token = await authService.getToken();
     final response = await http.get(
-      Uri.parse('$baseUrl/some-endpoint'),
+      Uri.parse('$baseUrl/task/getall'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer $token',
       },
     );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<dynamic>.from(data['data']);
+    } else {
+      throw Exception('Erro ao carregar as tarefas');
+    }
+  }
+
+  Future<Map<String, dynamic>> getTaskById(String id) async {
+    final token = await authService.getToken();
+    final response = await http.get(
+      Uri.parse('$baseUrl/task/details'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Falha ao carregar a tarefa');
+    }
+  }
+
+  Future<Map<String, dynamic>> createTask(String title, String description, String status) async {
+    final token = await authService.getToken();
+    final response = await http.post(
+      Uri.parse('$baseUrl/tasks'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'title': title,
+        'description': description,
+        'status': status,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Falha ao criar a tarefa');
+    }
   }
 }
